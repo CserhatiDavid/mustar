c8f2e5d08d1bac37777a68a6b5ed15acc48be830
Cserhati <CserhatiDavid4444@gmail.com>
2023-04-20 09:04:18 +0200

    almost ready

--- src/app.js
+++ src/app.js
@@ -8,3 +8,42 @@
 * Licenc: GNU GPL
 * Nem vállalunk felelősséget a rossz jegyekért
 */
+var aSide=document.querySelector("#aSide");
+var bSide=document.querySelector("#bSide");
+var cSide=document.querySelector("#cSide");
+var calcButton=document.querySelector("#calcButton");
+var result=document.querySelector("#result")
+
+    if (calcButton) {
+        calcButton.addEventListener("click",()=>{
+        workInput();
+        })
+    }
+function workInput() {
+    let aSideGood=checkInput(aSide.value);
+    let bSideGood=checkInput(bSide.value);
+    let cSideGood=checkInput(cSide.value);
+    if (aSideGood&&bSideGood&&cSideGood) {
+        //alert("jó")
+        let A=Number(aSide.value);
+        let B=Number(bSide.value);
+        let C=Number(cSide.value);
+        let volume=calcVolume(A,B,C);
+        result.value=volume+" cm3";
+    }else{
+        alert("hibás adat")
+    }
+    
+    //console.log(typeof aSide.value)
+    //console.log(typeof bSide.value)
+    //console.log(typeof cSide.value)
+    
+}
+
+function calcVolume(A,B,C) {
+    return A*B*C;
+}
+function checkInput(input) {
+    let res=input.match(/^[0-9]+$/);
+    return res;
+}
